3631d99212774e6cc7f7a5102aee5b246efab0e1
Lincoln Gadéa <lincoln@glinc.com.br>
2020-10-17 22:37:45 -0300

    Adicionado controles ao slide

---
+++ script.js
@@ -0,0 +1,33 @@
+let totalSlides = document.querySelectorAll(".slider--item").length; //retorna quantidade de elementos com a classe
+let currentSlide = 0;
+
+document.querySelector(".slider--width").style.width = 
+    `calc(100vw * ${totalSlides})`;
+
+document.querySelector(".slider--controls").style.height = 
+    `${document.querySelector('.slider').clientHeight}px`;
+
+function goPrev(){
+    currentSlide--;
+    if(currentSlide <0){
+        currentSlide = totalSlides - 1;
+    }
+    updateMargin();
+}
+
+function goNext(){
+    currentSlide++;
+    if(currentSlide > (totalSlides - 1)){
+        currentSlide = 0;
+    }
+    updateMargin();
+}
+
+function updateMargin(){
+    let newMargin = (currentSlide * document.body.clientWidth);
+    document.querySelector('.slider--width').style.marginLeft = `-${newMargin}px`;
+    //console.log(newMargin);
+}
+
+setInterval(goNext,3000);
+
